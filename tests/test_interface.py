b405170d7450f0d4a7532627ea973ec4735f4535
Tamir Bahar <tamir.bahar@gmail.com>
2025-08-20 14:41:09 +0300

    Refactoring

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import inspect
 import rich
 from types import FunctionType, MemberDescriptorType
@@ -6,58 +7,24 @@ from typing import Any, Callable
 METHOD_MARKER_ATTRIBUTE_NAME = "__implements__"
 
 
+
+
 class InterfaceMeta(type):
     def __new__(mcls, name, bases, namespace: dict[str, Any], /, **kwargs):
         # We know that all methods in an interface will be distinct, so there will
         # never be conflicts here.
-        interface_definitions = {
-            value: name
-            for name, value in namespace.items()
-            if inspect.isfunction(value)
-        }
-        # We'll need the values later for type-checking, but for a first go, we can do with names alone.
-        interface_names = {
-            name for name, value in namespace.items() if inspect.isfunction(value)
-        }
 
-        class _Adapter:
-            def __init__(self, obj):
-                """
-                1. Ensure the obj has an implementation.
-                    This can be by being registered, or having
-                    registration markers on all for all the interface methods
-                2. Create members for mapping from the interface names to the
-                    obj methods.
-                """
-                # Currently we only support direct implementations, not implement-for.
-                obj_implementations = set()
-                for name, value in inspect.getmembers(obj):
-                    # If a method of the object implements an interface method,
-                    # it will have that method registered in it's `__implements__`
-                    # member.
-                    member_implements = getattr(
-                        value, METHOD_MARKER_ATTRIBUTE_NAME, set()
-                    )
-                    for marker in member_implements:
-                        # First, ensure we're checking the right class
-                        if not isinstance(marker, MemberDescriptorType):
-                            raise TypeError(
-                                f"Expected member_descriptor, got {type(marker)}"
-                            )
-                        if marker.__objclass__ is not type(self):
-                            # Not a marker for this interface. Ignore it.
-                            continue
-                        if marker.__name__ not in interface_names:
-                            # This should _never_ happen.
-                            raise NameError(
-                                f"Where the fuck did that come from? {marker.__name__}"
-                            )
-                        # Need to use names here, as we have a member descriptor and not the actual function!
-                        setattr(self, marker.__name__, value)
-                        obj_implementations.add(marker.__name__)
-
-                if interface_names != obj_implementations:
-                    raise TypeError(f"Interface not implemented fully! {type(obj)}")
+        def _interface_init(self:Interface, impl):
+            """
+            1. Ensure the obj has an implementation.
+                This can be by being registered, or having
+                registration markers on all for all the interface methods
+            2. Create members for mapping from the interface names to the
+                obj methods.
+            """
+            impl_mapping = _collect_implementation(impl, type(self))
+            for name, value in impl_mapping.items():
+                setattr(self, name, value)
 
         # Create a new namespace with the modifications we need
         new_namespace = {}
@@ -66,19 +33,68 @@ class InterfaceMeta(type):
                 continue
             new_namespace[key] = value
 
-        slots = tuple(interface_definitions.values())
-        new_namespace["__slots__"] = slots
-        new_namespace["__init__"] = _Adapter.__init__
+        interface_definition = {
+            name: value
+            for name, value in namespace.items()
+            if inspect.isfunction(value)
+        }
 
+        slots = tuple(interface_definition.keys())
+        new_namespace["__slots__"] = slots
+        new_namespace["__init__"] = _interface_init
+        new_namespace["__interface_definition__"] = interface_definition
         return super().__new__(mcls, name, bases, new_namespace, **kwargs)
 
 
 class Interface(metaclass=InterfaceMeta):
-    pass
+    __interface_definition__:dict[str, Callable]
+
+def _get_interface_definition(interface:type[Interface])->dict[str, Callable]:
+    return interface.__interface_definition__
+
+def _typecheck_method(impl_method, interface_method)->bool:
+    return True
+
+def _collect_implementation(impl, interface:type[Interface]):
+    interface_definition = _get_interface_definition(interface)
+    impl_mapping = {}
+    for name, value in inspect.getmembers(impl):
+        # If a method of the object implements an interface method,
+        # it will have that method registered in it's `__implements__`
+        # member.
+        member_implements = getattr(
+            value, METHOD_MARKER_ATTRIBUTE_NAME, set()
+        )
+        for marker in member_implements:
+            # First, ensure we're checking the right class
+            if not isinstance(marker, MemberDescriptorType):
+                raise TypeError(
+                    f"Expected member_descriptor, got {type(marker)}"
+                )
+            if marker.__objclass__ is not interface:
+                # Not a marker for this interface. Ignore it.
+                continue
+            if marker.__name__ not in interface_definition:
+                # This should _never_ happen.
+                raise NameError(
+                    f"Where the fuck did that come from? {marker.__name__}"
+                )
+            if not _typecheck_method(value, interface_definition[marker.__name__]):
+                raise TypeError(f"Method mismatch!")
+            # Need to use names here, as we have a member descriptor and not the actual function!
+            impl_mapping[marker.__name__] = value
+    return impl_mapping
 
 
 def _implements_class(interface: type[Interface]):
-    # TODO: Register!
+    """
+    Here we want to create the name mapping in advance, to save time
+    on class instantiation.
+
+    Additionally, we wanna ensure it implements the interface.
+    """
+    # First, ensure we implement the interface!
+
     return lambda x: x
 
 
@@ -135,9 +151,5 @@ class _:
 
 def test_fully_explicit():
     mallard = Mallard()
-    rich.inspect(mallard.quack, all=True)
-    for m in mallard.quack.__implements__:
-        rich.inspect(m, all=True)
-        print(m.__objclass__)
     duck = Duck(mallard)
     duck.quack()
