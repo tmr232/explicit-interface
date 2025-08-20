bd3463d33bfd293430bf718a9dcfa6ba813836a6
Tamir Bahar <tamir.bahar@gmail.com>
2025-08-20 16:57:52 +0300

    Simplify tests

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -3,30 +3,126 @@ import pytest
 from explicit_interface.explicit import Interface, implements
 
 
+class IDog(Interface):
+    def bark(self): ...
+
+    def bite(self): ...
+
+
+def test_full_implementation():
+    @implements(IDog)
+    class Dog:
+        @implements(IDog.bark)
+        def bark(self):
+            pass
+
+        @implements(IDog.bite)
+        def bite(self):
+            pass
+
+    d = IDog(Dog())
+    d.bark()
+
+
+def test_partial_implementation():
+    with pytest.raises(TypeError):
+
+        @implements(IDog)
+        class PartialDog:
+            @implements(IDog.bark)
+            def bark(self):
+                pass
+
+
+def test_different_name():
+    @implements(IDog)
+    class Dog:
+        @implements(IDog.bark)
+        def speak(self):
+            pass
+
+        @implements(IDog.bite)
+        def bite(self):
+            pass
+
+    d = IDog(Dog())
+    d.bark()
+
+
+def test_has_other_methods():
+    @implements(IDog)
+    class Dog:
+        def __init__(self):
+            pass
+
+        @implements(IDog.bark)
+        def bark(self):
+            pass
+
+        @implements(IDog.bite)
+        def bite(self):
+            pass
+
+        def walk(self):
+            pass
+
+
+def test_implicit_full_implementation():
+    class Dog:
+        @implements(IDog.bark)
+        def bark(self):
+            pass
+
+        @implements(IDog.bite)
+        def bite(self):
+            pass
+
+    d = IDog(Dog())
+    d.bark()
+
+
+def test_with_args():
+    class Speaker(Interface):
+        def speak(self, words): ...
+
+    @implements(Speaker)
+    class Greeter:
+        @implements(Speaker.speak)
+        def greet(self, someone):
+            return f"Hello, {someone}!"
+
+    greeter = Greeter()
+    speaker = Speaker(greeter)
+    someone = "World"
+    assert greeter.greet(someone) == speaker.speak(someone)
+
+
+def test_conflicting_interfaces():
+    class SinglePrinter(Interface):
+        def print(self, msg): ...
+
+    class DoublePrinter(Interface):
+        def print(self, msg1, msg2): ...
+
+    @implements(SinglePrinter)
+    @implements(DoublePrinter)
+    class Impl:
+        @implements(SinglePrinter.print)
+        def print_single(self, msg):
+            print(msg)
+
+        @implements(DoublePrinter.print)
+        def print_double(self, msg1, msg2):
+            self.print_single(msg1)
+            self.print_single(msg2)
+
+    impl = Impl()
+    SinglePrinter(impl).print("a")
+    DoublePrinter(impl).print("a", "b")
+
+
 class TestImplementFor:
-    class IDog(Interface):
-        def bark(self): ...
-        def bite(self): ...
-
-    @pytest.fixture(name="IDog")
-    def _idog_fixture(self):
-        return self.IDog
-
-    @pytest.mark.xfail(reason="I don't know how to implement this yet.")
-    def test_pure_mapping(self, IDog: type[IDog]):
-        class Dog:
-            def speak(self):
-                pass
-
-            def eat(self):
-                pass
-
-        @implements(IDog, for_=Dog)
-        class _:
-            bark = Dog.speak
-            bite = Dog.eat
-
-    def test_explicit(self, IDog: type[IDog]):
+    def test_explicit(self):
         class Dog:
             def speak(self):
                 pass
@@ -44,106 +140,16 @@ class TestImplementFor:
         d = IDog(Dog())
         d.bark()
 
-
-class TestExplicit:
-    class IDog(Interface):
-        def bark(self): ...
-        def bite(self): ...
-
-    @pytest.fixture(name="IDog")
-    def _idog_fixture(self):
-        return self.IDog
-
-    def test_full_implementation(self, IDog: type[IDog]):
-        @implements(IDog)
+    @pytest.mark.xfail(reason="I don't know how to implement this yet.")
+    def test_pure_mapping(self):
         class Dog:
-            @implements(IDog.bark)
-            def bark(self):
-                pass
-
-            @implements(IDog.bite)
-            def bite(self):
-                pass
-
-        d = IDog(Dog())
-        d.bark()
-
-    def test_partial_implementation(self, IDog: type[IDog]):
-        with pytest.raises(TypeError):
-
-            @implements(IDog)
-            class PartialDog:
-                @implements(IDog.bark)
-                def bark(self):
-                    pass
-
-    def test_different_name(self, IDog: type[IDog]):
-        @implements(IDog)
-        class Dog:
-            @implements(IDog.bark)
             def speak(self):
                 pass
 
-            @implements(IDog.bite)
-            def bite(self):
+            def eat(self):
                 pass
 
-        d = IDog(Dog())
-        d.bark()
-
-    def test_has_other_methods(self, IDog: type[IDog]):
-        @implements(IDog)
-        class Dog:
-            def __init__(self):
-                pass
-
-            @implements(IDog.bark)
-            def bark(self):
-                pass
-
-            @implements(IDog.bite)
-            def bite(self):
-                pass
-
-            def walk(self):
-                pass
-
-
-class TestImplicit:
-    class IDog(Interface):
-        def bark(self): ...
-        def bite(self): ...
-
-    @pytest.fixture(name="IDog")
-    def _idog_fixture(self):
-        return self.IDog
-
-    def test_full_implementation(self, IDog: type[IDog]):
-        class Dog:
-            @implements(IDog.bark)
-            def bark(self):
-                pass
-
-            @implements(IDog.bite)
-            def bite(self):
-                pass
-
-        d = IDog(Dog())
-        d.bark()
-
-
-class Speaker(Interface):
-    def speak(self, words): ...
-
-
-def test_with_args():
-    @implements(Speaker)
-    class Greeter:
-        @implements(Speaker.speak)
-        def greet(self, someone):
-            return f"Hello, {someone}!"
-
-    greeter = Greeter()
-    speaker = Speaker(greeter)
-    someone = "World"
-    assert greeter.greet(someone) == speaker.speak(someone)
+        @implements(IDog, for_=Dog)
+        class _:
+            bark = Dog.speak
+            bite = Dog.eat
