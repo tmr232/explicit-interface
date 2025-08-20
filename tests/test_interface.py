b5654b8cc2d9dd35d80cc641c4f88a4a3c2f4d2c
Tamir Bahar <tamir.bahar@gmail.com>
2025-08-20 12:35:34 +0300

    Start prototyping. Currently with an interface baseclass, but I think a decorator would probably be more straightforward. After all, we _never_ instantiate the `Duck` class, not _really_.

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,4 +1,35 @@
+import inspect
+import rich
+from types import FunctionType
+
+class Interface:
+    pass
+
+def _implements_class(interface:type[Interface]):
+    # TODO: Register!
+    return lambda x:x
+
+def _implements_method(method:FunctionType):
+    def _decorator[F:FunctionType](f:F)->F:
+        f.__implements__ = getattr(f, "__implements__", set()) | {method}
+        return f
+    return _decorator
+
+def implements(interface_or_method: type[Interface] | FunctionType):
+    if isinstance(interface_or_method, type) and issubclass(interface_or_method, Interface):
+        return _implements_class(interface_or_method)
+    return _implements_method(interface_or_method)
+
+def implement(interface, for_):
+    return lambda x:x
+
 class Duck(Interface):
+    def __init__(self, obj):
+        """
+        We need to:
+        1. Find all the interface methods in the object
+        2. Create a mapping from the interface to those methods
+        """
     def quack(self): ...
 
 
@@ -24,9 +55,6 @@ class Dog:
 class _:
     quack = Dog.bark
 
-
-def main():
-
-
-if __name__ == "__main__":
-    main()
+def test_fully_explicit():
+    duck = Duck(Mallard())
+    duck.quack()
